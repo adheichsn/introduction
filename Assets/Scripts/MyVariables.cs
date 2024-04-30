c06f6be25379b665290567827d82963023dc632b
adheichsn <melodyslsbll90@gmail.com>
2024-04-30 14:48:57 +0700

    first commit

---
+++ Assets/Scripts/MyVariables.cs
@@ -0,0 +1,34 @@
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+// using TMPro;
+
+// public class MyVariables : MonoBehaviour
+// {
+//     // Variables
+//     // Just a scratchpad for playing with variables
+//     private int Health: '100';
+
+//     private string messageForTextView = "Aku lagi belajar bahasa C#";
+
+//     public string firstName;
+
+//     public string lastName;
+
+//     private const int PlayerScore = 100;
+
+//     private TextMeshProUGUI textMeshPro;
+//     // Start is called before the first frame update
+//     void Start()
+//     {
+//         Debug.Log("hello");
+//         textMeshPro = GetComponent<TextMeshProUGUI>();
+//         textMeshPro.text = $"Hello, {firstName} {lastName}!";
+//     }
+
+//     // Update is called once per frame
+//     void Update()
+//     {
+//         // PlayerScore = 200;
+//     }
+// }
